eff9edc9d7270ca6de5e366e20202d470e93f0b7
balaclava <yangabrielogy@gmail.com>
2023-06-28 18:15:49 -0300

    feat: classe Store abstraída

--- requests.js
+++ requests.js
@@ -1,4 +1,4 @@
-import axios from "https://cdn.skypack.dev/axios"
+import { StoreBase } from "../controladoUtils"
 
 /**
  * @author
@@ -7,15 +7,7 @@ import axios from "https://cdn.skypack.dev/axios"
  * GitHub: https://github.com/controlado
  */
 
-export class Store {
-    constructor() {
-        this.url = null
-        this.token = null
-        this.summoner = null
-        this.session = axios.create()
-        this.auth()
-    }
-
+export class Store extends StoreBase {
     /**
      * Compra os campeões que da array recebida.
      * 
@@ -63,62 +55,7 @@ export class Store {
     }
 
     async getAvailableChampions() {
-        const endpoint = "/storefront/v3/view/champions"
-        const response = await this.request("GET", endpoint)
+        const response = await this.request("GET", "/storefront/v3/view/champions")
         return response.data
-    }
-
-    /**
-     * Faz uma requisição para a loja.
-     *
-     * @async
-     * @function
-     * @summary Deve ser chamada após a conclusão do {@link auth}.
-     * @param {String} method - Método HTTP da requisição, como `GET`.
-     * @param {String} endpoint - Endpoint da requisição para a loja.
-     * @param {JSON} [requestBody] - Parâmetro opcional, corpo da requisição.
-     * @return {Promise<Response>} Resposta da requisição.
-     */
-    async request(method, endpoint, requestBody = undefined) {
-        const requestParams = {
-            "method": method,
-            "headers": {
-                "Authorization": `Bearer ${this.token}`
-            }
-        }
-        if (requestBody) { requestParams.data = requestBody }
-        return await this.session.request(this.url + endpoint, requestParams)
-    }
-
-    /**
-     * Autentica a classe, definindo os atributos da instância.
-     * 
-     * @async
-     * @function
-     * @summary Essa função deve ser chamada antes de utilizar a classe.
-     */
-    async auth() {
-        this.url = await this.getStoreUrl()
-        this.token = await this.getSummonerToken()
-        this.summoner = await this.getSummonerData()
-    }
-
-    async getStoreUrl() {
-        const endpoint = "/lol-store/v1/getStoreUrl"
-        const response = await fetch(endpoint)
-        return await response.json()
-    }
-
-    async getSummonerToken() {
-        const endpoint = "/lol-rso-auth/v1/authorization/access-token"
-        const response = await fetch(endpoint)
-        const responseData = await response.json()
-        return responseData.token
-    }
-
-    async getSummonerData() {
-        const endpoint = "/lol-summoner/v1/current-summoner"
-        const response = await fetch(endpoint)
-        return await response.json()
     }
 }
