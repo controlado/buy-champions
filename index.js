33cca27834faa22a72c235c6e35957f36f29fe8f
balaclava <yangabrielogy@gmail.com>
2023-07-04 19:25:54 -0300

    refactor: código formatado
    tipagem
    documentação
    ponto e vírgulas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { addRoutines } from "../controladoUtils"
-import { Store } from "./requests"
+import { addRoutines } from "../controladoUtils";
+import { Store } from "./requests";
 
 /**
  * @author balaclava
@@ -12,39 +12,39 @@ export const plugin = {
   "name": "Buy Champions",
   "url": "https://github.com/controlado/buy-champions",
   "version": "1.0.1",
-}
-const buttonId = "buy-450-champions-button"
+};
+const buttonId = "buy-450-champions-button";
 
 const onMutation = () => {
-  const frameStore = document.querySelector("#rcp-fe-lol-store-iframe > iframe")
-  const storeDocument = frameStore?.contentDocument.documentElement
-  if (!frameStore || storeDocument.querySelector(`#${buttonId}`)) { return }
+  const frameStore = document.querySelector("#rcp-fe-lol-store-iframe > iframe");
+  const storeDocument = frameStore?.contentDocument.documentElement;
+  if (!frameStore || storeDocument.querySelector(`#${buttonId}`)) { return; }
 
-  const store = new Store() // autenticando a loja
+  const store = new Store(); // autenticando a loja
 
-  // criação do botão pra comprar os campeões
-  const buyChampionButton = document.createElement("lol-uikit-flat-button")
-  buyChampionButton.id = buttonId
-  buyChampionButton.textContent = "450 EA"
-  buyChampionButton.style.marginRight = "18px"
+  // criação do botão para comprar os campeões
+  const buyChampionButton = document.createElement("lol-uikit-flat-button");
+  buyChampionButton.id = buttonId;
+  buyChampionButton.textContent = "450 EA";
+  buyChampionButton.style.marginRight = "18px";
 
   // callback do botão de compra
   buyChampionButton.onclick = async () => {
-    buyChampionButton.setAttribute("disabled", "true")
+    buyChampionButton.setAttribute("disabled", "true");
     try {
-      const availableChampions = await store.getAvailableChampionsByCost(450)
-      if (availableChampions.length > 0) { await store.buyChampions(availableChampions) }
+      const availableChampions = await store.getAvailableChampionsByCost(450);
+      if (availableChampions.length > 0) { await store.buyChampions(availableChampions); }
     }
-    catch (error) { console.error(`${plugin.name}:`, error) }
-    finally { buyChampionButton.removeAttribute("disabled") }
-  }
+    catch (error) { console.error(`${plugin.name}:`, error); }
+    finally { buyChampionButton.removeAttribute("disabled"); }
+  };
 
   // selecionar o local onde os botões vão ser colocados
-  const navBar = storeDocument.querySelector(".nav.nav-right")
-  navBar.insertBefore(buyChampionButton, navBar.firstChild)
-}
+  const navBar = storeDocument.querySelector(".nav.nav-right");
+  navBar.insertBefore(buyChampionButton, navBar.firstChild);
+};
 
 window.addEventListener("load", () => {
-  console.debug(`${plugin.name}: Report bugs to Balaclava#1912`)
-  addRoutines(onMutation)
-})
+  console.debug(`${plugin.name}: Report bugs to Balaclava#1912`);
+  addRoutines(onMutation);
+});
--- requests.js
+++ requests.js
@@ -1,4 +1,4 @@
-import { StoreBase } from "../controladoUtils"
+import { StoreBase } from "../controladoUtils";
 
 /**
  * @author balaclava
@@ -8,54 +8,54 @@ import { StoreBase } from "../controladoUtils"
  */
 
 export class Store extends StoreBase {
-    /**
-     * Compra os campeões que da array recebida.
-     * 
-     * @async
-     * @function
-     * @summary Deve ser chamada após a conclusão do {@link auth}.
-     * @param {JSON[]} champions - Objetos devem possuir `itemId` e `ip`.
-     * @return {Promise<Response>} Resposta da solicitação.
-     */
-    async buyChampions(champions) {
-        const items = champions.map(
-            champion => (
-                {
-                    "inventoryType": "CHAMPION",
-                    "itemId": champion.itemId,
-                    "ipCost": champion.ip,
-                    "quantity": 1
-                }
-            )
-        )
-        const requestBody = { "accountId": this.summoner.accountId, "items": items }
-        return await this.request("POST", "/storefront/v3/purchase", requestBody)
-    }
-
-    /**
-     * Retorna os campeões disponíveis na loja, que possuem o custo recebido.
-     * 
-     * @async
-     * @function
-     * @summary Deve ser chamada após a conclusão do {@link auth}.
-     * @param {Number} cost - Custo dos campeões que devem ser retornados.
-     * @return {Promise<JSON[]>} Array de campeões disponíveis.
-     */
-    async getAvailableChampionsByCost(cost) {
-        const playerChampions = await this.getAvailableChampions()
-        const availableChampions = []
-
-        for (const champion of playerChampions.catalog) {
-            if (!champion.owned && champion.ip == cost) {
-                availableChampions.push(champion)
+  /**
+   * Compra os campeões que da array recebida.
+   *
+   * @async
+   * @function
+   * @summary Deve ser chamada após a conclusão do {@link auth}.
+   * @param {Object[]} champions - Objetos devem possuir `itemId` e `ip`.
+   * @return {Promise<Response>} Resposta da solicitação.
+   */
+  async buyChampions(champions) {
+    const items = champions.map(
+        champion => (
+            {
+              "inventoryType": "CHAMPION",
+              "itemId": champion.itemId,
+              "ipCost": champion.ip,
+              "quantity": 1,
             }
-        }
+        ),
+    );
+    const requestBody = { "accountId": this.summoner.accountId, "items": items };
+    return await this.request("POST", "/storefront/v3/purchase", requestBody);
+  }
 
-        return availableChampions
+  /**
+   * Retorna os campeões disponíveis na loja, que possuem o custo recebido.
+   *
+   * @async
+   * @function
+   * @summary Deve ser chamada após a conclusão do {@link auth}.
+   * @param {number} cost - Custo dos campeões que devem ser retornados.
+   * @return {Promise<Object[]>} Array de campeões disponíveis.
+   */
+  async getAvailableChampionsByCost(cost) {
+    const playerChampions = await this.getAvailableChampions();
+    const availableChampions = [];
+
+    for (const champion of playerChampions.catalog) {
+      if (!champion.owned && champion.ip == cost) {
+        availableChampions.push(champion);
+      }
     }
 
-    async getAvailableChampions() {
-        const response = await this.request("GET", "/storefront/v3/view/champions")
-        return response.data
-    }
+    return availableChampions;
+  }
+
+  async getAvailableChampions() {
+    const response = await this.request("GET", "/storefront/v3/view/champions");
+    return response.data;
+  }
 }
