6af1a1cd76f120302b8acf6ca7bfcb9379794dfe
Yan Gabriel <yangabrielogy@gmail.com>
2023-07-23 00:13:21 -0300

    fix: index adicionado

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author balaclava
+ * @name buy-champions
+ * @link https://github.com/controlado/buy-champions
+ * @description Buy champions automatically! 🐧
+ */
+
+import "https://cdn.skypack.dev/buy-champions@latest?min";
